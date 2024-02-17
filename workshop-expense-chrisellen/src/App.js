e02dc5c55d0bb80bd12999aa817abec3920550c0
chrisellenbegontes <chrisellenbegontes@ckcm.edu.ph>
2024-02-17 15:29:05 +0800

    1st commit

---
+++ workshop-expense-chrisellen/src/App.js
@@ -0,0 +1,30 @@
+
+function App() {
+  return (
+    <div>
+    <form>
+      <textarea cols="30" rows="10"></textarea>
+      <input type="number" />
+      <input type="date" />
+      <button>Save</button>
+    </form>
+    <h2>My Expenses</h2>
+
+<table width="100%">
+       <thead>
+          <tr>
+           <th>Id</th>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+
+        <tbody>
+        </tbody>
+      </table>
+  </div>
+  );
+}
+
+export default App;
